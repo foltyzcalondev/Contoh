a3b49cbae66436c63f5a8dfbb618cbd2e3324333
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-02-24 17:27:17 +0700

    Add files via upload

---
+++ Python/contoh/README.md
@@ -0,0 +1,24 @@
+# **Contoh v1.0.3**
+[Logo library Contoh](https://tirta.ct.ws/aset/gambar/contoh_library_python.png)
+
+Ini adalah contoh library python yang paling simpel, sederhana, dan digunakan sebagai sampel/contoh.
+Ditujukan untuk logging, testing, eksperimen, dll.
+> Contoh Penggunaan:
+```{python}
+  from contoh import Contoh
+  from contoh import contoh
+
+  c = Contoh()
+  c.contoh() # untuk menampilkan pesan sederhana.
+  # atau bisa langsung: contoh()
+  contoh.sapa() # untuk menyapa anda dengan hangat :)
+```
+__Catatan: Apabila library ini terinstall, maka terdapat alat baris perintah (CLI) yang akan ditambahkan di sistem terminal anda__, yaitu:
+`$ contoh`
+
+## Fitur utama:
+* Debugging dan logging sederhana
+* Menyapa dengan hangat kepada anda
+* Memberi pengetahuan acak mengenai pemrograman dan coding :)
+
+**Sekian, terimakasih banyak😬🙏**
---
+++ Python/contoh/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    deskripsi = file.read()
+
+setup(
+  name = "contoh",
+  version = "1.0.3",
+  description = "Contoh library Python.",
+  long_description = deskripsi,
+  long_description_content_type = "text/markdown",
+  author = "Ahmad Daniar",
+  author_email = "foltyzxzhen@gmail.com",
+  url = "https://github.com/foltyzcalondev/Contoh/Python",
+  license = "MIT",
+  classifiers = [
+     "Development Status :: 5 - Production/Stable",
+     "Intended Audience :: Developers",
+     "License :: OSI Approved :: MIT License",
+     "Programming Language :: Python :: 3",
+     "Programming Language :: Python :: 3.6",
+     "Programming Language :: Python :: 3.7",
+     "Programming Language :: Python :: 3.8",
+     "Programming Language :: Python :: 3.9",
+  ],
+  keywords = "contoh library python sederhana pertama coba percobaan eksperimen peretasan pengembangan sampel misal top atas",
+  packages = find_packages(),
+  install_requires = [
+     "rich >= 1.0.0",
+     "rich-cli >= 1.0.0",
+     "pyfiglet >= 1.0.0",
+  ],
+  extras_requires = {
+     "dev" : [
+        "pytest",
+        "flake8",
+     ],
+  },
+  entry_points = {
+    "console_scripts" : [
+      "contoh = utama:contoh",
+    ],
+  },
+  include_package_data = True,
+  zip_file = True,
+)
