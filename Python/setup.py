4d4f4dd27428dc4757ba6e52577a8d05259d623f
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-02-24 17:00:02 +0700

    Create setup.py

---
+++ Python/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = "contoh",
+  version = "1.0.2",
+  description = "Contoh library Python.",
+  long_description = deskripsi,
+  long_description_content_type = "text/markdown",
+  author = "Ahmad Daniar",
+)
