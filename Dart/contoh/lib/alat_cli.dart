206fa12f526428e4d916f0e8d27d354accba7748
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-03-02 06:27:49 +0700

    Create alat_cli.dart

---
+++ Dart/contoh/lib/alat_cli.dart
@@ -0,0 +1,11 @@
+import "dart:io";
+import "dart:core";
+import "dart:async";
+
+void main() {
+   print(
+
+   );
+   print("\x1b[1;33mIni adalah contoh library Dart :D!\x1b[0m");
+   print("[💡] Untuk dokumentasi lengkap, kunjungi situs: \x1b[1;34mhttps://github.com/foltyzcalondev/Contoh\x1b[0m :)");
+}
