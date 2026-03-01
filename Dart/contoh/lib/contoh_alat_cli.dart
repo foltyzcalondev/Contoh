3ffc91820d2376f96c2b9d8b24dbe76fc3cdd499
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-03-02 06:34:02 +0700

    Update and rename alat_cli.dart to contoh_alat_cli.dart

--- Dart/contoh/lib/contoh_alat_cli.dart
+++ Dart/contoh/lib/contoh_alat_cli.dart
@@ -4,7 +4,11 @@ import "dart:async";
 
 void main() {
    print(
-
+           ______            __        __  
+    / ____/___  ____  / /_____  / /_ 
+   / /   / __ \/ __ \/ __/ __ \/ __ \
+  / /___/ /_/ / / / / /_/ /_/ / / / /
+  \____/\____/_/ /_/\__/\____/_/ /_/ 
    );
    print("\x1b[1;33mIni adalah contoh library Dart :D!\x1b[0m");
    print("[💡] Untuk dokumentasi lengkap, kunjungi situs: \x1b[1;34mhttps://github.com/foltyzcalondev/Contoh\x1b[0m :)");
