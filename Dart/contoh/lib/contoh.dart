7cd5ab5856ee3c043a026b0b1d1336ea6c194cd5
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-03-02 05:59:34 +0700

    Create contoh.dart

---
+++ Dart/contoh/lib/contoh.dart
@@ -0,0 +1,17 @@
+library contoh;
+
+import "dart:io";
+import "dart:core";
+import "dart:convert";
+
+void contoh() {
+   print("Ini adalah contoh library Dart :)");
+}
+
+void halo() {
+   print("Halo Dart!");
+}
+
+void sapa(nama, umur, alamat) {
+   print("\x1b[34mHalo!, nama saya $nama, umur saya $umur tahun, dan alamat saya di $alamat.\x1b[0m";
+}
